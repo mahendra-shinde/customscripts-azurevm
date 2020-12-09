b5f33dbed01f19cb61fd5e67a7c7d8e055273b2e
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-12-09 18:41:55 +0530

    docker-install

--- windows/installscript.ps1
+++ windows/installscript.ps1
@@ -1,12 +1,18 @@
+param (
+    $UserName  
+)
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart 
 Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
 
-$script = "choco install docker-desktop git vscode -y; logoff" 
-$trig = New-ScheduledTaskTrigger -AtLogOn -Once
-$task = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $script -AsJob
-new-scheduledtask -Action $task -Trigger $trig 
+New-LocalGroup -Name docker-users -Description "Users of Docker Desktop"
+Add-LocalGroupMember -Group 'docker-users' -Member $UserName
+
+choco install docker-desktop git vscode -y;
+
+$trig = New-ScheduledTaskTrigger -AtLogOn 
+$task = New-ScheduledTaskAction -Execute "C:\Program Files\Docker\Docker\Docker Desktop.exe" 
+Register-ScheduledTask -TaskName start-docker -Force -Action $task -Trigger $trig
 
 #Trigger a restart to enable hyper-v and containers
 Restart-Computer -Force
